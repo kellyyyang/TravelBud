12371767094c39c61abaa8a95b20d5bffe31ee45
kellyyyyang <kelyang@hmc.edu>
2022-06-28 11:06:12 -0700

    SearchFragment shows a list of users.

--- app/src/main/java/com/codepath/travelbud/UserSearchAdapter.java
+++ app/src/main/java/com/codepath/travelbud/UserSearchAdapter.java
@@ -1,4 +1,70 @@
 package com.codepath.travelbud;
 
-public class UserSearchAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.parse.ParseFile;
+import com.parse.ParseUser;
+
+import java.util.List;
+
+public class UserSearchAdapter extends RecyclerView.Adapter<UserSearchAdapter.ViewHolder> {
+
+    private Context context;
+    private List<ParseUser> users;
+    private String KEY_PROFILEPIC = "profilePic";
+
+    public UserSearchAdapter(Context context, List<ParseUser> users) {
+        this.context = context;
+        this.users = users;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_user_search, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        ParseUser user = users.get(position);
+        holder.bind(user);
+    }
+
+    @Override
+    public int getItemCount() {
+        return users.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView tvUsernameSearch;
+        private ImageView ivProfilePicSearch;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            tvUsernameSearch = itemView.findViewById(R.id.tvUsernameSearch);
+            ivProfilePicSearch = itemView.findViewById(R.id.ivProfilePicSearch);
+
+        }
+
+        public void bind(ParseUser user) {
+            // bind the user data to the view elements
+            tvUsernameSearch.setText(user.getUsername());
+            ParseFile profilePic = user.getParseFile(KEY_PROFILEPIC);
+            if (profilePic != null) {
+                Glide.with(context).load(profilePic.getUrl()).into(ivProfilePicSearch);
+            }
+        }
+    }
 }
--- app/src/main/java/com/codepath/travelbud/fragments/SearchFragment.java
+++ app/src/main/java/com/codepath/travelbud/fragments/SearchFragment.java
@@ -2,59 +2,39 @@ package com.codepath.travelbud.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.codepath.travelbud.R;
+import com.codepath.travelbud.UserSearchAdapter;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link SearchFragment#newInstance} factory method to
- * create an instance of this fragment.
  */
 public class SearchFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    public static final String TAG = "SearchFragment";
+    private RecyclerView rvUsersSearch;
+    private UserSearchAdapter adapter;
+    private List<ParseUser> allUsers;
 
     public SearchFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SearchFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SearchFragment newInstance(String param1, String param2) {
-        SearchFragment fragment = new SearchFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -62,5 +42,39 @@ public class SearchFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_search, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        rvUsersSearch = view.findViewById(R.id.rvUsersSearch);
+
+        allUsers = new ArrayList<>();
+        adapter = new UserSearchAdapter(getContext(), allUsers);
+
+        rvUsersSearch.setAdapter(adapter);
+        rvUsersSearch.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        queryUsers();
+
+    }
+
+    private void queryUsers() {
+        ParseQuery<ParseUser> query = ParseQuery.getQuery("_User");
+        query.findInBackground(new FindCallback<ParseUser>() {
+            @Override
+            public void done(List<ParseUser> users, ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "Issue with getting users", e);
+                    return;
+                }
+                for (ParseUser user : users ) {
+                    Log.i(TAG, "User: " + user.getUsername());
+                }
+                allUsers.addAll(users);
+                adapter.notifyDataSetChanged();
+            }
+        });
     }
 }
