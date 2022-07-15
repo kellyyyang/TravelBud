4e20dd24046eeb62cecedb8c7da0bc3fa42b83a2
kellyyyang <kelyang@hmc.edu>
2022-07-15 11:53:10 -0700

    Added a viewpager for Explore/Search page.

---
+++ app/src/main/java/com/codepath/travelbud/fragments/SearchSliderFragment.java
@@ -0,0 +1,28 @@
+package com.codepath.travelbud.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.codepath.travelbud.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SearchSliderFragment extends Fragment {
+
+    public SearchSliderFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_search_slider, container, false);
+    }
+}
